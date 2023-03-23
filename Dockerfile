56b36dbc1b3cbd57d8b388150bd4c7562aee413d
Markus <markusmorgenstern87@outlook.de>
2023-03-23 21:40:20 +0100

    initial

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+﻿FROM mcr.microsoft.com/dotnet/runtime:7.0 AS base
+WORKDIR /app
+
+FROM mcr.microsoft.com/dotnet/sdk:7.0 AS build
+WORKDIR /src
+COPY ["BackendTest.csproj", "./"]
+RUN dotnet restore "BackendTest.csproj"
+COPY . .
+WORKDIR "/src/"
+RUN dotnet build "BackendTest.csproj" -c Release -o /app/build
+
+WORKDIR /app/build
+EXPOSE 5000
+ENTRYPOINT ["dotnet", "BackendTest.dll"]
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using BackendTest.Hubs;
+using Microsoft.AspNetCore.SignalR;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddSignalR().AddMessagePackProtocol();
+builder.Services.AddCors(o =>
+{
+    o.AddDefaultPolicy(b =>
+    {
+        b.WithOrigins("http://localhost:4200").AllowAnyHeader().WithMethods("GET", "POST").AllowCredentials();
+    });
+});
+var app = builder.Build();
+app.UseCors();
+app.MapHub<ChatHub>("/chatHub");
+app.Run("http://0.0.0.0:5000");
